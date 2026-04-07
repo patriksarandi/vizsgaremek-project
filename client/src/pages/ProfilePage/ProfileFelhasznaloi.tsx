a9b39c2271661902a89b86b7169ae0f9dbaf658e
Patrik Sarandi <31217801+patriks2005@users.noreply.github.com>
2026-04-07 21:53:59 +0200

    Profile Page frontend oldali módosítások

--- client/src/pages/ProfilePage/ProfileFelhasznaloi.tsx
+++ client/src/pages/ProfilePage/ProfileFelhasznaloi.tsx
@@ -1,57 +1,95 @@
 ﻿import { Button, Form } from "react-bootstrap";
 import { Autentikacio } from "../../components/AuthContext";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const ProfileFelhasznaloi = () => {
   const { user } = Autentikacio();
 
-  const [vezeteknev, setVezeteknev] = useState(user.Vezeteknev);
-  const [keresztnev, setKeresztnev] = useState(user.Keresztnev);
-  const [telefonszam, setTelefonszam] = useState(user.Telefonszam);
-  const [email, setEmail] = useState(user.Email);
-  
+  const vezeteknevRef = useRef(null);
+  const keresztnevRef = useRef(null);
+  const telefonszamRef = useRef(null);
+  const emailRef = useRef(null);
+
+  const handleUpdateNev = async () => {
+    try {
+      const teljesNev = {
+        vezeteknev: vezeteknevRef.current.value,
+        keresztnev: keresztnevRef.current.value,
+      };
+
+      const response = await fetch(
+        `http://localhost:7777/vevo/${user.VevoID - 1}/teljes-nev`,
+        {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(teljesNev),
+        },
+      );
+
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.message || "Hiba történt a mentés során.");
+
+      console.log("Sikeres frissítés:", data);
+      alert("Név sikeresen módosítva!");
+    } catch (error: any) {
+      console.error("Hiba:", error.message);
+      alert("Hiba: " + error.message);
+    }
+  };
 
   return (
     <>
       <h4>Elérhetőségek</h4>
       <Form>
-        <Form.Label>Vezetéknév</Form.Label>
-        <Form.Control
-          value={vezeteknev}
-          placeholder={vezeteknev ? vezeteknev : "Nincs megadva"}
-        ></Form.Control>
+        <Form.Group className="mb-3">
+          <Form.Label>Vezetéknév</Form.Label>
+          <Form.Control
+            ref={vezeteknevRef}
+            defaultValue={user.Vezeteknev}
+          ></Form.Control>
+        </Form.Group>
 
-        <Form.Label>Keresztnév</Form.Label>
-        <Form.Control
-          value={keresztnev}
-          placeholder={keresztnev ? keresztnev : "Nincs megadva"}
-        ></Form.Control>
+        <Form.Group className="mb-3">
+          <Form.Label>Keresztnév</Form.Label>
+          <Form.Control
+            ref={keresztnevRef}
+            defaultValue={user.Keresztnev}
+          ></Form.Control>
+        </Form.Group>
 
-        <Button>Módosítás</Button>
+        <Button onClick={handleUpdateNev}>Módosítás</Button>
       </Form>
 
+      <hr />
       <h4>Telefonszám</h4>
       <Form>
-        <Form.Label>Telefonszám</Form.Label>
-        <Form.Control
-          value={telefonszam}
-          placeholder={telefonszam ? telefonszam : "Nincs megadva"}
-        ></Form.Control>
+        <Form.Group className="mb-3">
+          <Form.Label>Telefonszám</Form.Label>
+          <Form.Control
+            ref={telefonszamRef}
+            defaultValue={user.Telefonszam}
+          ></Form.Control>
+        </Form.Group>
 
         <Button>Telefonszám Módosítás</Button>
       </Form>
 
+      <hr />
       <h4>E-mail cím</h4>
       <Form>
-        <Form.Label>E-mail cím</Form.Label>
-        <Form.Control
-          value={email}
-          placeholder={email ? email : "Nincs megadva"}
-        ></Form.Control>
+        <Form.Group className="mb-3">
+          <Form.Label>E-mail cím</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            defaultValue={user.VevoEmail}>
+          </Form.Control>
+        </Form.Group>
 
         <Button>Telefonszám Módosítás</Button>
       </Form>
 
+      <hr/>
       <h4>Jelszó</h4>
       <Form>
         <Form.Label>Jelenlegi jelszó</Form.Label>
