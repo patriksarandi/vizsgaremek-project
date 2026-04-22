ee4ae28859ca3e471b75130f6db157bd9719312d
Patrik Sarandi <31217801+patriks2005@users.noreply.github.com>
2026-04-22 23:23:46 +0200

    Kosár lekérésének és végösszeg számításának tesztelése

--- server/src/rendeles/rendeles.service.spec.ts
+++ server/src/rendeles/rendeles.service.spec.ts
@@ -1,8 +1,6 @@
-﻿import { PrismaService } from "src/prisma.service";
-import { RendelesService } from "./rendeles.service";
-import { Test, TestingModule } from "@nestjs/testing";
-
-
+﻿import { PrismaService } from 'src/prisma.service';
+import { RendelesService } from './rendeles.service';
+import { Test, TestingModule } from '@nestjs/testing';
 
 const mockPrisma = {
   fizetesiKosar: {
@@ -48,16 +46,52 @@ describe('RendelesService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('createFizetesiKosar', () => {
-    it('sikeresen létrehozza/frissíti a kosarat', async () => {
-      const dto = { VevoID: 1 };
-      mockPrisma.fizetesiKosar.upsert.mockResolvedValue({ KosarID: 1, VevoID: 1 });
-      
-      const result = await service.createFizetesiKosar(dto as any);
+  describe('RendelesService', () => {
+    let service: RendelesService;
+    let db: PrismaService;
 
-      expect(db.fizetesiKosar.upsert).toHaveBeenCalled();
-      expect(result.ujFizetesiKosar.VevoID).toEqual(1);
-      expect(result.message).toContain('sikeresen létrehozva');
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          RendelesService,
+          { provide: PrismaService, useValue: mockPrisma },
+        ],
+      }).compile();
+
+      service = module.get<RendelesService>(RendelesService);
+      db = module.get<PrismaService>(PrismaService);
+    });
+
+    describe('createFizetesiKosar', () => {
+      it('sikeresen létrehozza/frissíti a kosarat', async () => {
+        const dto = { VevoID: 1 };
+        mockPrisma.fizetesiKosar.upsert.mockResolvedValue({
+          KosarID: 1,
+          VevoID: 1,
+        });
+        const result = await service.createFizetesiKosar(dto as any);
+        expect(db.fizetesiKosar.upsert).toHaveBeenCalled();
+        expect(result.ujFizetesiKosar.VevoID).toEqual(1);
+      });
+    });
+
+    describe('findKosarTetelByVevoId', () => {
+      it('kiszámolja az összeget', async () => {
+        const mockKosar = {
+          VevoID: 1,
+          Tetelek: [{ TetelMennyiseg: 2, Termek: { TermekAr: 1000 } }],
+        };
+        mockPrisma.fizetesiKosar.findUnique.mockResolvedValue(mockKosar);
+        const result = await service.findKosarTetelByVevoId(1) as any;
+
+        expect(result.Vegosszeg).toBe(2000);
+      });
+
+      it('hibát dob, ha nincs kosár', async () => {
+        mockPrisma.fizetesiKosar.findUnique.mockResolvedValue(null);
+        const result = await service.findKosarTetelByVevoId(999) as any;
+        expect(result.message).toBe('A kosár nem található!');
+      });
     });
   });
 });
