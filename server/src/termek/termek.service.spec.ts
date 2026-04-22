5257788b8a539f507a4e84ed584e435f1942e16d
Patrik Sarandi <31217801+patriks2005@users.noreply.github.com>
2026-04-23 00:31:19 +0200

    Árszűrő és Kategória szűrő tesztelés

--- server/src/termek/termek.service.spec.ts
+++ server/src/termek/termek.service.spec.ts
@@ -35,7 +35,7 @@ describe('TermekService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('findAll', () => {
+  describe('findAll - Keresés', () => {
     it('keresési szűrés szavak alapján', async () => {
       const query = { search: 'Fender Gitár' };
       mockPrisma.termek.findMany.mockResolvedValue([]);
@@ -52,6 +52,43 @@ describe('TermekService', () => {
 
       expect(callArgs.where.AND[1].OR).toContainEqual({
         TermekNev: { contains: 'Gitár' },
+      });
+    });
+  });
+
+  describe('findAll - Ár és Lapozás', () => {
+    it('ársáv és lapozás számítása', async () => {
+      const query = { page: 2, limit: 5, minPrice: 1000, maxPrice: 5000 };
+      mockPrisma.termek.findMany.mockResolvedValue([]);
+      await service.findAll(query);
+      const callArgs = mockPrisma.termek.findMany.mock.calls[0][0];
+
+      expect(callArgs.skip).toBe(5);
+      expect(callArgs.take).toBe(5);
+      expect(callArgs.where.TermekAr).toEqual({
+        gte: 1000,
+        lte: 5000,
+      });
+    });
+  });
+
+  describe('findAll - Kategória', () => {
+    it('azonosító alapján szűr, ha számot kap', async () => {
+      const query = { category: '1,2,3' };
+      await service.findAll(query);
+
+      const callArgs = mockPrisma.termek.findMany.mock.calls[0][0];
+      expect(callArgs.where.KategoriaID).toEqual({ in: [1, 2, 3] });
+    });
+
+    it('név alapján szűr, ha szöveget kap', async () => {
+      const query = { category: 'Basszusgitár, Gitár, Vonós' };
+      await service.findAll(query);
+
+      const callArgs = mockPrisma.termek.findMany.mock.calls[0][0];
+
+      expect(callArgs.where.Kategoria.Nev).toEqual({
+        in: ['Basszusgitár', 'Gitár', 'Vonós'],
       });
     });
   });
