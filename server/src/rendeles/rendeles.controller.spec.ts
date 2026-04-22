2a530d3ef3e47a18ef5412b4d7eeba4f5208418f
Patrik Sarandi <31217801+patriks2005@users.noreply.github.com>
2026-04-22 23:50:19 +0200

    Rendelési mock setup

---
+++ server/src/rendeles/rendeles.controller.spec.ts
@@ -0,0 +1,35 @@
+﻿import { Test, TestingModule } from "@nestjs/testing";
+import { RendelesController } from "./rendeles.controller"
+import { RendelesService } from "./rendeles.service";
+
+describe('RendelesController', () => {
+    let controller: RendelesController;
+    let service: RendelesService;
+
+    const mockRendelesService = {
+        createFizetesiKosar: jest.fn(),
+        findAllFizetesiKosar: jest.fn(),
+        findRendelesekByVevo: jest.fn(),
+        createKosarTetel: jest.fn(),
+        removeKosarTetel: jest.fn(),
+        createRendeles: jest.fn(),
+        removeKosar: jest.fn(),
+        updateKosarTetelMennyiseg: jest.fn(),
+    };
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [RendelesController],
+            providers: [
+                { provide: RendelesService, useValue: mockRendelesService },
+            ],
+        }).compile();
+
+        controller = module.get<RendelesController>(RendelesController);
+        service = module.get<RendelesService>(RendelesService);
+    });
+
+    it('definiálva', () => {
+        expect(controller).toBeDefined();
+    })
+})
